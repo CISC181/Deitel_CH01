9bd5dcd02ae208fc82ff948792fdb29c35f24475
tstbag <tstbag@verizon.net>
2020-03-14 21:42:22 -0400

    initial commit

---
+++ DeitelCode_Chapter01/src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module DeitelCode {
+	
+	
+	exports ch01.Painter;
+
+	requires javafx.base;
+	requires javafx.controls;
+	requires javafx.graphics;
+	requires javafx.fxml;
+	
+	opens ch01.Painter;
+}
